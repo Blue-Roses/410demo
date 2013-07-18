204cdec0d4b76a5c99e58bc09d775facfd4101a3
Blue-Roses <mnurse631@stu.aii.edu>
2013-07-18 16:10:28 -0400

    Beginning LAB of inclues

---
+++ header.inc.php
@@ -0,0 +1,12 @@
+  <header>
+ 	<h1> Demo Widgets </h1>
+  	<nav>
+    	<a href="" title="" class="home_lnk">Home</a> 
+        <a href="" title="" class="about_lnk">About</a> 
+        <a href="" title="" class="product_lnk">Product</a> 
+        <a href="" title="" class="contact_lnk">Contact</a> 
+  	</nav>
+  </header> 
+
+       
+        
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php $page = "home"; ?>
+
+<?php require_once('includes/top.inc.php'); ?>
+
+<body>
+<div class="gridContainer clearfix">
+
+  <?php require_once('includes/header.inc.php'); ?> 
+  
+  <section class="main">
+  <h2>Welcome </h2>
+  	<figure>
+    	<img src="images.jpg" alt="yellow and red floral pic" /> 
+     	<figcaption></figcaption>  
+  	</figure> 
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's type specimen book. It has survived not only five centuries, but also the leap into electronic types	</p> 
+    
+  </section> 
+  	    
+  <?php require_once('includes/sidebar.inc.php'); ?> 
+  
+  <?php require_once('includes/footer.inc.php'); ?>
+  
+  
+</div>
+
+</body>
+</html>
